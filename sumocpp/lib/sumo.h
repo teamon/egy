539f63fee119c6fb60e4a4af4c36b089bd54ab7b
teamon <i@teamon.eu>
2009-02-05 21:27:37 +0100

    config/header

--- sumocpp/lib/sumo.h
+++ sumocpp/lib/sumo.h
@@ -1,48 +1,50 @@
 #ifndef SUMO_H_
 #define SUMO_H_
 
-// Opis wyprowadzeń procesora
-// 
-// PORTA
-//     0 - DIST1 - czujnik odległości
-//     1 - DIST2 - czujnik odległości
-//     2 - DIST3 - czujnik odległości
-//     3 - DIST4 - czujnik odległości
-//     4 - DIST5 - czujnik odległości
-//     5 - DIST6 - czujnik odległości
-//     6 - SERVO1 - wejście serwomechanizmu
-//     7 - SERVO2 - wejście serwomechanizmu
-// 
-// PORTB
-//     0 - GROUND1 - czujnik podłoża
-//     1 - GROUND2 - czujnik podłoża
-//     2 - GROUND3 - czujnik podłoża
-//     3 - GROUND4 - czujnik podłoża
-//     4 - LEDS - dioda świecąca
-//     5 - MOSI - używany do programowania
-//     6 - MISO - używany do programowania
-//     7 - SCK - używany do programowania
-// 
-// PORTC
-//     0 - LED1 - dioda świecąca
-//     1 - LED2 - dioda świecąca
-//     2 - LED3 - dioda świecąca
-//     3 - LED4 - dioda świecąca
-//     4 - LED5 - dioda świecąca
-//     5 - LED6 - dioda świecąca
-//     6 - LED7/SERVO4 - dioda świecąca/wejście serwomechanizmu
-//     7 - LED8/SERVO3 - dioda świecąca/wejście serwomechanizmu
-// 
-// PORTD
-//     0 - RXD - RS232
-//     1 - TXD - RS232
-//     2 - SWITCH1 - przycisk
-//     3 - SWITCH2 - przycisk
-//     4 - M1_PWM - start/stop silnika
-//     5 - M2_PWM - start/stop silnika
-//     6 - M1_DIR - kierunek obrotów silnika
-//     7 - M2_DIR - kierunek obrotów silnika
-// 
+/* Opis wyprowadzeń procesora
+ *
+ * PORTA
+ *   0 - DIST1 - czujnik odległości
+ *   1 - DIST2 - czujnik odległości
+ *   2 - DIST3 - czujnik odległości
+ *   3 - DIST4 - czujnik odległości
+ *   4 - AIN1 - wejscie analogowe
+ *   5 - AIN2 - wejscie analogowe
+ *   6 - AIN3 - wejście analogowe
+ *   7 - AIN4 - wejście analogowe
+ * 
+ * PORTB
+ *   0 - GROUND1 - czujnik podłoża
+ *   1 - GROUND2 - czujnik podłoża
+ *   2 - GROUND3 - czujnik podłoża
+ *   3 - GROUND4 - czujnik podłoża
+ *   4 - SLED_LE - wyjscie sterownika diod
+ *   5 - MOSI - używany do programowania
+ *   6 - MISO - używany do programowania
+ *   7 - SCK - używany do programowania
+ * 
+ * PORTC
+ *   0 - M1_DIR - kierunek obrotów silnika
+ *   1 - M2_DIR - kierunek obrotów silnika
+ *   2 - SERVO1 - wyjśćie serwomechanizmu
+ *   3 - SERVO2 - wyjśćie serwomechanizmu
+ *   4 - SERVO3 - wyjśćie serwomechanizmu
+ *   5 - SERVO4 - wyjśćie serwomechanizmu
+ *   6 - SLED_CLK - wyjśćie sterownika diod
+ *   7 - SLED_SDI - wyjśćie sterownika diod
+ *  
+ * PORTD
+ *   0 - RXD - port szeregowy
+ *   1 - TXD - port szeregowy
+ *   2 - SWITCH1 - przycisk
+ *   3 - SWITCH2 - przycisk
+ *   4 - M1_PWM - start/stop silnika
+ *   5 - M2_PWM - start/stop silnika
+ *   6 - SIG - sygnał do wykorzystania
+ *   7 - GRD_PWM - regulacja mocy diod czujników podłoża
+ *
+ */
+
 
 
 // Częstotliwość kwarcu
@@ -58,11 +60,11 @@
 // Linia portu wyjścia PWM silnika 1
 #define MOTOR1_PWM_PIN 4
 // Port wyjścia kierunku silnika 1
-#define MOTOR1_DIR_PORT PORTD
+#define MOTOR1_DIR_PORT PORTC
 // Rejestr kierunku portu wyjścia kierunku silnika 1
-#define MOTOR1_DIR_DDR DDRD
+#define MOTOR1_DIR_DDR DDRC
 // Linia portu wyjścia kierunku silnika 1
-#define MOTOR1_DIR_PIN 7
+#define MOTOR1_DIR_PIN 0
 
 
 // Port wyjścia PWM silnika 2
@@ -72,40 +74,40 @@
 // Linia portu wyjścia PWM silnika 2
 #define MOTOR2_PWM_PIN 5
 // Port wyjścia kierunku silnika 2
-#define MOTOR2_DIR_PORT PORTD
+#define MOTOR2_DIR_PORT PORTC
 // Rejestr kierunku portu wyjścia kierunku silnika 2
-#define MOTOR2_DIR_DDR DDRD
+#define MOTOR2_DIR_DDR DDRC
 // Linia portu wyjścia kierunku silnika 2
-#define MOTOR2_DIR_PIN 6
+#define MOTOR2_DIR_PIN 1
 
 
 // Port serwomechanozmu 1
-#define SERVO1_PORT PORTA
+#define SERVO1_PORT PORTC
 // Rejestr kierunku portu serwomechanozmu 1
-#define SERVO1_DDR DDRA
+#define SERVO1_DDR DDRC
 // Linia portu serwomechanozmu 1
-#define SERVO1_PIN 6
+#define SERVO1_PIN 2
 
 // Port serwomechanozmu 2
-#define SERVO2_PORT PORTA
+#define SERVO2_PORT PORTC
 // Rejestr kierunku portu serwomechanozmu 2
-#define SERVO2_DDR DDRA
+#define SERVO2_DDR DDRC
 // Linia portu serwomechanozmu 1
-#define SERVO2_PIN 7
+#define SERVO2_PIN 3
 
 // Port serwomechanozmu 3
 #define SERVO3_PORT PORTC
 // Rejestr kierunku portu serwomechanozmu 3
 #define SERVO3_DDR DDRC
 // Linia portu serwomechanozmu 3
-#define SERVO3_PIN 6
+#define SERVO3_PIN 4
 
 // Port serwomechanozmu 4
 #define SERVO4_PORT PORTC
 // Rejestr kierunku portu serwomechanozmu 4
 #define SERVO4_DDR DDRC
 // Linia portu serwomechanozmu 4
-#define SERVO4_PIN 7
+#define SERVO4_PIN 5
 
 
 // Port czujników odległości
@@ -120,6 +122,8 @@
 #define DIST4_PIN 3
 #define DIST5_PIN 4
 #define DIST6_PIN 5
+#define DIST7_PIN 6
+#define DIST8_PIN 7
 
 
 // Port czujników podłoża
@@ -129,12 +133,17 @@
 #define GROUND_DDR DDRB
 // Rejestr wejściowy portu czujników podłaża
 #define GROUND_PINPORT PINB
-
 // Linie czujników podłoża
 #define GROUND1_PIN 0
 #define GROUND2_PIN 1
 #define GROUND3_PIN 2
 #define GROUND4_PIN 3
+// Port mocy czujników podłoża
+#define GROUND_PWN_PORT PORTD
+// Rejestr kierunku portu mocy czujników podłaża
+#define GROUND_PWM_DDR DDRD
+// Linia portu mocy czujników podłoża
+#define GROUND_PWM 7
 
 
 // Port przycisku 1
@@ -156,26 +165,25 @@
 #define SWITCH2_PIN 3
 
 
-// Port specjalnej diody LED
-#define LEDS_PORT PORTB
-// Rejestr kierunku portu specjalnej diody LED
-#define LEDS_DDR DDRB
-// Linia diody specjalnej
-#define LEDS_PIN 4
-// Port diagnostycznych diod LED
-#define LED_PORT PORTC
-// Rejestr kierunku portu diagnostycznych diod LED
-#define LED_DDR DDRC
+// Port danych sterownika diod LED
+#define SLED_SDI_PORT PORTC
+// Rejestr kierunku portu danych sterownika diod LED
+#define SLED_SDI_DDR DDRC
+// Linia portu danych sterownika diod LED
+#define SLED_SDI_PIN 7
+// Port danych sterownika diod LED
+#define SLED_CLK_PORT PORTC
+// Rejestr kierunku portu danych sterownika diod LED
+#define SLED_CLK_DDR DDRC
+// Linia portu danych sterownika diod LED
+#define SLED_CLK_PIN 6
+// Port zatrzasku sterownika diod LED
+#define SLED_LE_PORT PORTB
+// Rejestr kierunku portu zatrzasku sterownika diod LED
+#define SLED_LE_DDR DDRB
+// Linia portu zatrzasku sterownika diod LED
+#define SLED_LE_PIN 4
 
-// Linie poszczególnych diod
-#define LED1_PIN 0
-#define LED2_PIN 1
-#define LED3_PIN 2
-#define LED4_PIN 3
-#define LED5_PIN 4
-#define LED6_PIN 5
-#define LED7_PIN 6
-#define LED8_PIN 7
 
 // Wartość preskalera timera0
 #define T0_PRESCALER 256
@@ -208,7 +216,25 @@
 
 // lib/led.cpp
 void led_init();
-void led_send(unsigned char value);
+void led_send();
+void led_set(unsigned int value);
+void led1_on();
+void led1_off();
+void led2_on();
+void led2_off();
+void led3_on();
+void led3_off();
+void led4_on();
+void led4_off();
+void led5_on();
+void led5_off();
+void led6_on();
+void led6_off();
+void led7_on();
+void led7_off();
+void led8_on();
+void led8_off();
+void led_delay();
 
 // lib/wait.cpp
 void wait_s(int s);
@@ -241,12 +267,20 @@ unsigned char dist(char id);
 void servo_init();
 void servo1_forward();
 void servo2_forward();
+void servo3_forward();
+void servo4_forward();
 void servo1_backward();
 void servo2_backward();
+void servo3_backward();
+void servo4_backward();
 void servo1_stop();
 void servo2_stop();
+void servo3_stop();
+void servo4_stop();
 void servo1_calibrate();
 void servo2_calibrate();
+void servo3_calibrate();
+void servo4_calibrate();
 
 // lib/usart.cpp
 void usart_init();
