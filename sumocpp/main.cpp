5f5446b8ef2a18551d51524e908c7101a24323a8
Piotr Sokolski <Pietras@macbook-pro-piotr-sokolski.local>
2009-04-21 11:42:58 +0200

    ucieczka z grd

--- sumocpp/main.cpp
+++ sumocpp/main.cpp
@@ -19,11 +19,11 @@
 
 
 #define ITIME 20
-#define DEBUG 1
+#define DEBUG 0
 #define WAIT 1000
 
 Queue q;
-bool front = 0;
+bool back = 0;
 bool zwarcie = 0;
 unsigned char zwarcieLen = 0;
 
@@ -33,9 +33,10 @@ void fikumiku(){
 	//bo czujniki nie sa zamieniane stronami, wiec wystarczy zamiana
 	//przod<->tyl
 	
-	front = !front;
-	motor[0].reverse = motor[1].reverse = front;
-	
+	back = !back;
+	motor[0].reverse = motor[1].reverse = back;
+	motor[0].setPower(0);
+	motor[1].setPower(0);
 	q.clear();
 }
 
@@ -75,19 +76,20 @@ void setTurn(char rad, float angle, char pri){
 }
 
 void unik(char pri){
+	return;
 	//zakladam ze zatrzymanie i zmiana kierunku to proces czasochlonny
 	//wiec daze do zachowania jak najmniejszej ilosci zatrzyman/zmian	
 	bool ktorym = 0;
 	int wyn = motor[0].getPower()*motor[1].getPower();
 	if (wyn > 0){
-		ktorym = (Dist[front][0] > Dist[front][1] && front == 0);//jezeli jest
+		ktorym = (Dist[back][0] > Dist[back][1] && back == 0);//jezeli jest
 		//szansa ze z jednej strony nic nie ma to zatrzymaj ten motor
 	}else if (wyn == 0){
 		//wybierz ten z mniejsza moca (czyli ten z zerem)
-		ktorym = (mabs(motor[0].getPower())>mabs(motor[1].getPower())) && front == 0;
+		ktorym = (mabs(motor[0].getPower())>mabs(motor[1].getPower())) && back == 0;
 	}else{
 		//wybierz ten z wieksza moca (czyli dodatni)
-		ktorym = (motor[0].getPower()<motor[1].getPower() && front==0);
+		ktorym = (motor[0].getPower()<motor[1].getPower() && back==0);
 	}
 	
 	//jeden z silnikow sie zatrzyma, 2 pojedzie do tylu
@@ -99,10 +101,10 @@ void unik(char pri){
 
 unsigned char getGround(){
 	unsigned char out=1;
-	if (ground1_detected()) out*=(!front)?2:5; // przod lewy
-	if (ground2_detected()) out*=(!front)?3:7; // przod prawy
-	if (ground3_detected()) out*=(!front)?5:2; // tyl lewy
-	if (ground4_detected()) out*=(!front)?7:3; // tyl prawy
+	if (ground1_detected()) out*=2; // przod lewy
+	if (ground2_detected()) out*=3; // przod prawy
+	if (ground3_detected()) out*=5; // tyl lewy
+	if (ground4_detected()) out*=7; // tyl prawy
 	return out;
 }
  
@@ -130,35 +132,37 @@ void planEscape(unsigned char grd, char fp, char bp){
 		else
 			moveStraight(20, 3);
 	}else{
-		if (grd%2==0 || grd%3==0){
+		if (grd!=14 && grd!=15 && 
+		((grd%2==0 || grd%3==0) && !back || 
+		(grd%5==0 || grd%7==0) && back)){
 			fikumiku();
 		}
 		switch(grd){
 			case 35: // tyl
 			case 6: // przod
 				moveStraight(400, 3);
-				setTurn(oneWheel, .25, 2);
+				setTurn(oneWheel, .125, 2);
 				return;
 			
 			case 2: // przod lewy
 			case 5: // tyl lewy
-				setTurn(lightTurn, .125, 3); // skret o 45 stopni w prawo
+				setTurn(lightTurn, ((back)?-1:1)*.125, 3); // skret o 45 stopni w prawo
 				moveStraight(200, 3);
 				return;
 			
 			case 3: // przod prawy
 			case 7: // tyl prawy
-				setTurn(lightTurn, -.125, 3); // skret o 45 stopni w prawo
+				setTurn(lightTurn, ((!back)?-1:1)*.125, 3); // skret o 45 stopni w prawo
 				moveStraight(200, 3);
 				return;
 				
-			case 10: // lewo
-				setTurn(oneWheel, .25, 3);
+			case 15: // lewo
+				setTurn(lightTurn, ((back)?-1:1)*.25, 3);
 				moveStraight(200, 3);
 				return;
 				
-			case 21: // prawo
-				setTurn(oneWheel, -.25, 3);
+			case 14: // prawo
+				setTurn(lightTurn, ((!back)?-1:1)*.25, 3);
 				moveStraight(200, 3);
 				return;
 		}
@@ -214,7 +218,7 @@ struct ValAndDist{
 	int val;
 };
 
-ValAndDist values[] = {
+/*ValAndDist values[] = {
 {10, 120},
 {15, 87},
 {20, 70},
@@ -236,14 +240,14 @@ char getDistance(int value, char probe){
 		int center = (left + n)/2;
 		if (values[center].val == value || n-left < 2){
 			return values[center].dist;
-		}else if (values[center].val < val){
+		}else if (values[center].val < value){
 			n = center;
 		}else{
 			left = center;
 		}
 	}
 	return -1;
-}
+}*/
 
 unsigned char ticks = 0;
 bool odliczanie = false;
@@ -251,6 +255,7 @@ bool hold = false, hold2 = false;
 char strategia = 0;
 int power = 1;
 bool preLoop(){
+	led_set(getGround());
 	if (DEBUG){
 		usart_write_byte(0);
 		if (usart_read_byte() == '!'){
@@ -286,8 +291,8 @@ bool preLoop(){
 }
 
 void loop(){
+	led_set(255*back);
 	kalmanize();
-	
 	if(DEBUG){
 		debug();
 		return;
@@ -306,8 +311,8 @@ void loop(){
 		//sprawdzam disty 3 i 6 i grd
 		unsigned char ground = getGround();
 
-		char fProbe = 0;//getProbe(front);
-		char bProbe = 0;//getProbe(!front);
+		fProbe = 0;//getProbe(back);
+		bProbe = 0;//getProbe(!back);
 		
 		if (fProbe == 2 || bProbe == 2){
 			zwarcie = true;
@@ -340,7 +345,7 @@ void loop(){
 		q.dec(1);
 	}else{
 		//szukaj
-		motor[0].setPower(60);
+		motor[0].setPower(100);
 		motor[1].setPower(100);
 	}	
 }
